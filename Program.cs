3cfb9b3b5e9ed1a614cd1591bd7fa77e799b5ce7
TelloVilla <engjvilla@gmail.com>
2023-08-09 12:26:52 -0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CombinedLetters
+{
+    public interface ILetterService
+    {
+        void CombineTwoLetters(string inputFile1, string inputFile2, string resultFile);
+    }
+
+    public class LetterService : ILetterService
+    {
+        public void CombineTwoLetters(string inputFile, string inputFile2, string resultFile)
+        {
+            return;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+
+        }
+    }
+}
